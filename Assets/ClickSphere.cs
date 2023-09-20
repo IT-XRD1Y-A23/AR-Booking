2b7b8fae039bb9b157d036a8123cf44a7bc08f99
Esben Fogh <esbenvensel.fogh@gmail.com>
2023-09-20 20:50:10 +0200

    Adding simple light script

--- Assets/ClickSphere.cs
+++ Assets/ClickSphere.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class ClickSphere : MonoBehaviour
 {
+    Light lightComp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+        print(gameObject.name);
+        lightComp = gameObject.AddComponent<Light>();
+        lightComp.color = Color.green;
+        lightComp.intensity = 1;
+
+    }
+    // sadasd
     // Update is called once per frame
     void Update()
     {
         
     }
-	
-		
+
+    void OnMouseDown(){
+
+        if(lightComp.enabled)
+        {
+            lightComp.enabled = false;
+        } else
+        {
+            lightComp.enabled = true;
+        }
+
+
+    }
+
+
+
+
 
 }
